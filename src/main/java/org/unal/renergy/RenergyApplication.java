28af27a366a64f4e35b4a1e339b4cac6073c841e
Kevin Jay <kjayh@unal.edu.co>
2025-03-12 11:48:36 -0500

    agregado spring boot

---
+++ src/main/java/org/unal/renergy/RenergyApplication.java
@@ -0,0 +1,13 @@
+package org.unal.renergy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RenergyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RenergyApplication.class, args);
+    }
+
+}

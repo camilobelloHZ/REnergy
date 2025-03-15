98757fa84e8f133b705798f24d57b6ed4720404e
Kevin Jay <kjayh@unal.edu.co>
2025-03-15 16:01:54 -0500

    creado codigo de ejemplo de conexion con jpa y comentarios

--- src/main/java/org/unal/renergy/CountryContinentEntity.java
+++ src/main/java/org/unal/renergy/CountryContinentEntity.java
@@ -1,4 +1,19 @@
 package org.unal.renergy;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+// Entidad que represneta el objeto guardado en base de datos
+@Entity
+@Data
+@Table( name = "entities")
 public class CountryContinentEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String name;
+
+    private boolean is_country;
 }
--- src/main/java/org/unal/renergy/CountryContinentRepository.java
+++ src/main/java/org/unal/renergy/CountryContinentRepository.java
@@ -1,4 +1,8 @@
 package org.unal.renergy;
 
-public class CountryContinentRepository {
-}
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+// Repositorio que accede a la informacion de la base de datos con el ORM Hibernate
+@Repository
+public interface CountryContinentRepository extends JpaRepository<CountryContinentEntity, Long> { }
